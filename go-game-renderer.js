527b95fd176f18a11068f8965547240c244da572
Cameron Dugan <cameron.dugan@protonmail.com>
2024-05-09 09:53:00 -0400

    better board

--- go-game-renderer.js
+++ go-game-renderer.js
@@ -1,11 +1,19 @@
 function init() {
+  drawBoard();
+}
+
+function pieceIndexToCanvas(i, markerSpacing, edgePadding) {
+  return i * markerSpacing + edgePadding;
+}
+
+function drawBoard() {
   const board = document.getElementById("GoGame");
   if (board == null || !board.getContext) {
     // avoid crash if method not exist
     return;
   }
 
-  const validGridSizes = [3, 5, 7, 9, 15, 21];
+  const validGridSizes = [9, 13, 19];
   const gridSize =
     validGridSizes[Math.floor(Math.random() * validGridSizes.length)];
 
@@ -17,26 +25,118 @@ function init() {
   // Draw the go board
   let canvas = board.getContext("2d");
   canvas.fillStyle = "#DCB069";
-
   canvas.fillRect(0, 0, board.width, board.height);
+  const boardBG = document.getElementById("boardBG");
+  canvas.drawImage(boardBG, 0, 0);
 
-  const edgePadding = boardPixels * (0.8 / gridSize);
+  // draw markers
+  const edgePadding = 30;
   const markerSize = 3;
   const innerBoardSize = board.width - edgePadding * 2;
   const markerSpacing = innerBoardSize / (gridSize - 1);
-  canvas.fillStyle = "#634F2F";
-  function indexToCanvas(i) {
-    return i * markerSpacing + edgePadding - markerSize / 2;
-  }
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      canvas.fillRect(
-        indexToCanvas(i),
-        indexToCanvas(j),
-        markerSize,
+  switch (gridSize) {
+    case 9:
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(2, markerSpacing, edgePadding),
+        pieceIndexToCanvas(2, markerSpacing, edgePadding),
         markerSize,
       );
-    }
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(2, markerSpacing, edgePadding),
+        pieceIndexToCanvas(6, markerSpacing, edgePadding),
+        markerSize,
+      );
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(6, markerSpacing, edgePadding),
+        pieceIndexToCanvas(2, markerSpacing, edgePadding),
+        markerSize,
+      );
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(6, markerSpacing, edgePadding),
+        pieceIndexToCanvas(6, markerSpacing, edgePadding),
+        markerSize,
+      );
+      break;
+    case 13:
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(3, markerSpacing, edgePadding),
+        pieceIndexToCanvas(3, markerSpacing, edgePadding),
+        markerSize,
+      );
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(3, markerSpacing, edgePadding),
+        pieceIndexToCanvas(9, markerSpacing, edgePadding),
+        markerSize,
+      );
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(9, markerSpacing, edgePadding),
+        pieceIndexToCanvas(3, markerSpacing, edgePadding),
+        markerSize,
+      );
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(9, markerSpacing, edgePadding),
+        pieceIndexToCanvas(9, markerSpacing, edgePadding),
+        markerSize,
+      );
+      drawMarker(
+        canvas,
+        pieceIndexToCanvas(6, markerSpacing, edgePadding),
+        pieceIndexToCanvas(6, markerSpacing, edgePadding),
+        markerSize,
+      );
+      break;
+    case 19:
+      for (let i = 3; i < 19; i += 6) {
+        for (let j = 3; j < 19; j += 6) {
+          drawMarker(
+            canvas,
+            pieceIndexToCanvas(i, markerSpacing, edgePadding),
+            pieceIndexToCanvas(j, markerSpacing, edgePadding),
+            markerSize,
+          );
+        }
+      }
+      break;
+  }
+  drawBoardLines(canvas, innerBoardSize, edgePadding, gridSize, markerSpacing);
+}
+
+// grid draw
+function drawBoardLines(
+  canvas,
+  innerBoardSize,
+  edgePadding,
+  gridSize,
+  markerSpacing,
+) {
+  canvas.strokeStyle = "#000000";
+  canvas.strokeRect(edgePadding, edgePadding, innerBoardSize, innerBoardSize);
+  for (let i = 1; i < gridSize - 1; i++) {
+    let x = pieceIndexToCanvas(i, markerSpacing, edgePadding);
+    drawLine(canvas, x, edgePadding, x, edgePadding + innerBoardSize);
+    drawLine(canvas, edgePadding, x, edgePadding + innerBoardSize, x);
+    console.log(i, x, edgePadding, edgePadding + innerBoardSize);
   }
 }
-init();
+
+function drawLine(canvas, x, y, x2, y2) {
+  canvas.beginPath();
+  canvas.moveTo(x, y);
+  canvas.lineTo(x2, y2);
+  canvas.stroke();
+}
+
+function drawMarker(canvas, x, y, w) {
+  canvas.fillStyle = "#000000";
+  canvas.beginPath();
+  canvas.arc(x, y, w, 0, 2 * Math.PI);
+  canvas.fill();
+}
