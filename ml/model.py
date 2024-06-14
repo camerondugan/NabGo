9d360d093008ccdbe58c9b067c84b9c691c1c68a
Josh Gifford <71138022+giffordj1atwit@users.noreply.github.com>
2024-06-14 17:07:48 -0400

    Delete ml/model.py

--- ml/model.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-results = model.train(data="C:\\Users\\giffordj1\\Desktop\\Senior Project\\NabGo\\Go Positions.v6-yolov8n-25epochs-resize640x640_aug3x.yolov8\\data.yaml", epochs=1, device=0)
-
-success = model.export(format="onnx")
