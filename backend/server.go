7e40f7106315fead901a8a77b52a89fbb5e5104b
giffordj1atwit <giffordj1@wit.edu>
2024-06-13 20:36:28 -0400

    Pass image to Go backend via form data

--- backend/server.go
+++ backend/server.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "https://nabgo.us")
+}
 
 func handleAuth(w http.ResponseWriter, req *http.Request, redirect string) {
 	verifier, challenge := generatePKCE()
@@ -52,12 +59,41 @@ func headers(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func handlePredict(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	r.ParseMultipartForm(32 << 20) // limit your max input length!
+	var buf bytes.Buffer
+	// in your case file would be fileupload
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	name := strings.Split(header.Filename, ".")
+	fmt.Printf("File name %s\n", name[0])
+	// Copy the file data to my buffer
+	io.Copy(&buf, file)
+	// do something with the contents...
+	// I normally have a struct defined and unmarshal into a struct, but this will
+	// work as an example
+	contents := buf.String()
+	fmt.Println(contents)
+	// I reset the buffer in case I want to use it again
+	// reduces memory allocations in more intense projects
+	buf.Reset()
+	// do something else
+	// etc write header
+	predict()
+	w.Write([]byte("done"))
+}
+
 func runServer() {
 	// set things to respond to
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/ui/signin", handleUiSignIn)
 	http.HandleFunc("/ui/signup", handleUiSignUp)
+	http.HandleFunc("/predict", handlePredict)
 	// define a server and what address it listens to
 	server := &http.Server{
 		Addr:        ":8888",
--- website/go-game-renderer.js
+++ website/go-game-renderer.js
@@ -25,7 +25,6 @@ function init() {
   trackTheMouse();
   drawBoard();
   placeExampleStones();
-  setupLoginButton();
 }
 
 function initWhiteStones() {
