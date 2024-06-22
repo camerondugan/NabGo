d60cf9ddae89cd05f16196849978686b77dbcbcd
Cameron Dugan <cameron.dugan@protonmail.com>
2024-06-22 13:03:13 -0400

    made python implementation as a tool
    run: python predict-server <file-name>
    whatever file will have a new file: <file-name>.json
    go will eventually read these and submit them back, and clean up the
    files as necessary

--- backend/predict-server.go
+++ backend/predict-server.go
@@ -3,10 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os/exec"
 )
 
 func predictResults(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "hello what do you have for me?\n")
+	// create two pipes: one for image (go to python)
+	// code here
+	// One for results (python to go)
+	// code there
+
+	// Call the Python script that uses these pipes
+	cmd := exec.Command("python", "../machine-learning/server-predict.py")
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Python command had err: \n" + err.Error())
+		panic("Python failed in some way")
+	}
+	fmt.Println(stdout)
 }
 
 func runPredictionServer() {
