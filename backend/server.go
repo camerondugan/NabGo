f993b3816e8f23b41e16eb265c1b4f4a50743b1e
Cameron Dugan <cameron.dugan@protonmail.com>
2024-07-18 15:19:55 -0400

    flip sgf

--- backend/server.go
+++ backend/server.go
@@ -118,9 +118,9 @@ func handleSgf(w http.ResponseWriter, req *http.Request) {
 			if stone == -1 { //null
 				continue
 			} else if stone == 0 { //black
-				sgfMaker.PlaceBlackPiece(x, y)
+				sgfMaker.PlaceBlackPiece(x, 19-y)
 			} else { // white
-				sgfMaker.PlaceWhitePiece(x, y)
+				sgfMaker.PlaceWhitePiece(x, 19-y)
 			}
 		}
 	}
